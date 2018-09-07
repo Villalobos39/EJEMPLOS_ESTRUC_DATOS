60fc7aa0f8f41b8d5fc5707078c354cfd0ea238c
DULCE VILLALOBOS <42878496+Villalobos39@users.noreply.github.com>
2018-09-06 23:01:29 -0700

    PRACTICA.01 TDA TIPOS DE DATOS ABSTRACTOS

---
+++ DISTANCIA.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRACT01.AXIOMA
+{
+    class DISTANCIA
+    {
+        Double a, b, c, d, r; // puntos de la recta 
+        public Double A
+        { get { return a; } set { a = value; } }
+        public Double B
+        { get { return b; } set { b = value; } }
+        public Double C
+        { get { return c; } set { c = value; } }
+        public Double D
+        { get { return d; } set { d = value; } }
+        public Double R
+        { get { return r; } set { r = value; } }
+
+        public DISTANCIA() { }
+        public DISTANCIA(Double a, Double c, Double b, Double d, Double r)
+        { a = A; b = B; c = C; d = D; r = R; }
+
+        public Double CALCULAR()
+        {           
+            return R = Math.Sqrt((Math.Pow(C-A,2)) + (Math.Pow(D-B,2)));
+        }
+        public void IMPRIMIR()
+        {
+            Console.Write(" \n DISTANCIAS ENTRE DOS PUNTOS \n ");
+            Console.Write("P (A,B ) ; ( "+ a + " , "+ b + " ) \n");
+            Console.Write("P (C,D ) ; ( "+ c + " , "+ d +" ) \n");
+            Double r = CALCULAR();
+            Console.Write("LA DISTANCIA ENTRE PUNTOS ES :  " + r);
+            Console.ReadKey();
+        }
+        ~DISTANCIA() { }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRACT01.AXIOMA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DISTANCIA P = new DISTANCIA();
+            string s;
+           
+            do
+            {
+                Console.Write("\n DISTANCIAS ENTRE DOS PUNTOS \n ");
+                Console.Write("\n INGRESA LAS CORDENADAS DE LOS PUNTOS : \n");
+                try
+                {                    
+                        Console.Write("A : "); P.A = Double.Parse(Console.ReadLine());
+                        Console.Write("B : "); P.B = Double.Parse(Console.ReadLine());
+                        Console.Write("C : "); P.C = Double.Parse(Console.ReadLine());
+                        Console.Write("D : "); P.D = Double.Parse(Console.ReadLine());                 
+                        Console.Clear();
+                }
+                catch (FormatException e) { Console.WriteLine(e.Message); }
+
+                finally
+                {                  
+                    P.IMPRIMIR();
+                   
+                }
+
+                Console.WriteLine("\n ____________________________________________\n");
+                Console.WriteLine("DESEA INGRESAR NUEVOS VALORES ? : (s/n) ");
+                s = Console.ReadLine(); // El programa pregunta si se desea volver a ejecutar
+            }
+            while (s == "s" || s == "S");
+            Console.Clear();
+        }
+    }   
+}
